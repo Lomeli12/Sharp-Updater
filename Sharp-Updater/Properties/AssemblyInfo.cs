2c5aedf57f17b43d5d98df82bd3dd05d9d979712
Anthony Lomeli <anthonylomeli11@gmail.com>
2011-10-31 11:12:57 -0700

    Quick changes. Nothing special

--- Sharp-Updater/Properties/AssemblyInfo.cs
+++ Sharp-Updater/Properties/AssemblyInfo.cs
@@ -1,17 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sharp-Updater")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Sharp-Updater; a Generic Updater running under .NET environment that uses plain xml files to check for newer versions of your software.")]
+[assembly: AssemblyDescription("Sharp-Updater; a Generic Updater running under .NET environment that uses plain xml files to check for newer versions of your software.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sharp-Updater")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Anthoy Lomeli")]
+[assembly: AssemblyProduct("Sharp-Updater; a Generic Updater running under .NET environment that uses plain xml files to check for newer versions of your software.")]
+[assembly: AssemblyCopyright("Copyright ©  2011 Anthony Lomeli")]
+[assembly: AssemblyTrademark("Anthony Lomeli")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
