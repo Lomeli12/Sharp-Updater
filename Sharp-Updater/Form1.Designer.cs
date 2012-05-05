af3c95b54fce362147bff62185d8139b4871643d
unknown <Anthony@Anthony-PC.(none)>
2012-05-04 22:00:46 -0700

    Some simple changes that make it more easier on the client end

--- Sharp-Updater/Form1.Designer.cs
+++ Sharp-Updater/Form1.Designer.cs
@@ -28,102 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(170, 80);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(46, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "No";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(118, 80);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(46, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Yes";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 51);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(204, 23);
-            this.progressBar1.TabIndex = 4;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(37, 80);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Cancel";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 35);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(228, 112);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+        	System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+        	this.label1 = new System.Windows.Forms.Label();
+        	this.label2 = new System.Windows.Forms.Label();
+        	this.button1 = new System.Windows.Forms.Button();
+        	this.button2 = new System.Windows.Forms.Button();
+        	this.progressBar1 = new System.Windows.Forms.ProgressBar();
+        	this.button3 = new System.Windows.Forms.Button();
+        	this.label3 = new System.Windows.Forms.Label();
+        	this.SuspendLayout();
+        	// 
+        	// label1
+        	// 
+        	this.label1.AutoSize = true;
+        	this.label1.Location = new System.Drawing.Point(12, 9);
+        	this.label1.Name = "label1";
+        	this.label1.Size = new System.Drawing.Size(35, 13);
+        	this.label1.TabIndex = 0;
+        	this.label1.Text = "label1";
+        	// 
+        	// label2
+        	// 
+        	this.label2.AutoSize = true;
+        	this.label2.Location = new System.Drawing.Point(12, 22);
+        	this.label2.Name = "label2";
+        	this.label2.Size = new System.Drawing.Size(35, 13);
+        	this.label2.TabIndex = 1;
+        	this.label2.Text = "label2";
+        	// 
+        	// button1
+        	// 
+        	this.button1.Location = new System.Drawing.Point(194, 80);
+        	this.button1.Name = "button1";
+        	this.button1.Size = new System.Drawing.Size(46, 23);
+        	this.button1.TabIndex = 2;
+        	this.button1.Text = "No";
+        	this.button1.UseVisualStyleBackColor = true;
+        	this.button1.Click += new System.EventHandler(this.button1_Click);
+        	// 
+        	// button2
+        	// 
+        	this.button2.Location = new System.Drawing.Point(142, 80);
+        	this.button2.Name = "button2";
+        	this.button2.Size = new System.Drawing.Size(46, 23);
+        	this.button2.TabIndex = 3;
+        	this.button2.Text = "Yes";
+        	this.button2.UseVisualStyleBackColor = true;
+        	this.button2.Click += new System.EventHandler(this.button2_Click);
+        	// 
+        	// progressBar1
+        	// 
+        	this.progressBar1.Location = new System.Drawing.Point(12, 51);
+        	this.progressBar1.Name = "progressBar1";
+        	this.progressBar1.Size = new System.Drawing.Size(228, 23);
+        	this.progressBar1.TabIndex = 4;
+        	// 
+        	// button3
+        	// 
+        	this.button3.Location = new System.Drawing.Point(61, 80);
+        	this.button3.Name = "button3";
+        	this.button3.Size = new System.Drawing.Size(75, 23);
+        	this.button3.TabIndex = 5;
+        	this.button3.Text = "Cancel";
+        	this.button3.UseVisualStyleBackColor = true;
+        	this.button3.Click += new System.EventHandler(this.button3_Click);
+        	// 
+        	// label3
+        	// 
+        	this.label3.AutoSize = true;
+        	this.label3.Location = new System.Drawing.Point(12, 35);
+        	this.label3.Name = "label3";
+        	this.label3.Size = new System.Drawing.Size(35, 13);
+        	this.label3.TabIndex = 6;
+        	this.label3.Text = "label3";
+        	// 
+        	// Form1
+        	// 
+        	this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        	this.ClientSize = new System.Drawing.Size(246, 112);
+        	this.Controls.Add(this.label3);
+        	this.Controls.Add(this.button3);
+        	this.Controls.Add(this.progressBar1);
+        	this.Controls.Add(this.button2);
+        	this.Controls.Add(this.button1);
+        	this.Controls.Add(this.label2);
+        	this.Controls.Add(this.label1);
+        	this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+        	this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+        	this.MaximizeBox = false;
+        	this.Name = "Form1";
+        	this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+        	this.Text = "Form1";
+        	this.Load += new System.EventHandler(this.Form1_Load);
+        	this.ResumeLayout(false);
+        	this.PerformLayout();
         }
 
         #endregion
--- Sharp-Updater/Form1.cs
+++ Sharp-Updater/Form1.cs
@@ -46,36 +46,59 @@ namespace Sharp_Updater
                 button1.Text = "Close";
             }
         }
+        
+        public double bytesIn
+        {
+        	get;
+        	set;
+        }
+        
+        public double totalBytes
+        {
+        	get;
+        	set;
+        }
 
         public void download_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
-            double bytesIn = double.Parse(e.BytesReceived.ToString());
-            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            bytesIn = double.Parse(e.BytesReceived.ToString());
+            totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
             double percentage = bytesIn / totalBytes * 100;
+            double downloaded = bytesIn / 1024;
+            double max = totalBytes / 1024;
             progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+            label3.Text = Math.Round(downloaded, 2) + "KB of " + Math.Round(max, 2) +"KB downloaded.";
         }
 
         public void download_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
-        {
-            string path = System.IO.Path.GetTempPath();
+        {   
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             XmlDocument current = new XmlDocument();
-            current.Load(Environment.CurrentDirectory + @"\Info.xml");
-            XmlNodeList pro = current.GetElementsByTagName("ProgramName");
+           	current.Load(Environment.CurrentDirectory + @"\Info.xml");
+           	XmlNodeList pro = current.GetElementsByTagName("ProgramName");
             XmlNodeList update = current.GetElementsByTagName("updatexml");
             string prog = pro[0].InnerText;
-            string updateurl = update[0].InnerText;
-            XmlDocument updater = new XmlDocument();
-            updater.Load(updateurl);
-            XmlNodeList load = updater.GetElementsByTagName("InstallerNAME");
-            string installer = load[0].InnerText;
-            MessageBox.Show("Update has been downloaded sucessfully. Please Close all instances of " + prog + " while update is in session.");
-            System.Diagnostics.Process.Start(path + installer);
+           	string updateurl = update[0].InnerText;
+           	XmlDocument updater = new XmlDocument();
+           	updater.Load(updateurl);
+           	XmlNodeList load = updater.GetElementsByTagName("InstallerNAME");
+           	string installer = load[0].InnerText;
+            if (bytesIn == totalBytes)
+            {
+            	MessageBox.Show("Update has been downloaded sucessfully. Please Close all instances of " + prog + " while update is in session.", "Update Complete");
+            	System.Diagnostics.Process.Start(path + @"\" + installer);
+            }
+            else if (bytesIn != totalBytes)
+            {
+            	MessageBox.Show("Update Canceled", "!WARNING!");
+        		System.IO.File.Delete(path + @"\" + installer);
+            }
             Application.Exit();
         }
-
+		WebClient download = new WebClient();
         private void button2_Click(object sender, EventArgs e)
         {
-            string path = System.IO.Path.GetTempPath();
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             button3.Visible = true;
             button1.Enabled = false;
             XmlDocument current = new XmlDocument();
@@ -94,21 +117,20 @@ namespace Sharp_Updater
             string newversion = ver2[0].InnerText;
             string installer = load[0].InnerText;
             string install = down[0].InnerText + load[0].InnerText;
-            WebClient download = new WebClient();
-            label1.Text = "Downloading update for " + prog + ".";
+            label1.Text = "Downloading version "+ newversion +" update for " + prog + ".";
             label2.Text = "Please wait...";
-            label3.Text = "Downloading Version " + newversion + " installer. ";
+            label3.Text = "";
             download.DownloadProgressChanged += new DownloadProgressChangedEventHandler(download_DownloadProgressChanged);
             download.DownloadFileCompleted += new AsyncCompletedEventHandler(download_DownloadFileCompleted);
             button2.Enabled = false;
-            if (System.IO.File.Exists(path + installer) == false)
+            if (System.IO.File.Exists(path + @"\" + installer) == false)
             {
-                download.DownloadFileAsync(new Uri(install), (path + installer));
+                download.DownloadFileAsync(new Uri(install), (path  + @"\" + installer));
             }
-            else if (System.IO.File.Exists(path + installer) == true)
+            else if (System.IO.File.Exists(path + @"\" + installer) == true)
             {
-                System.IO.File.Delete(path + installer);
-                download.DownloadFileAsync(new Uri(install), (path + installer));
+                System.IO.File.Delete(path + @"\" + installer);
+                download.DownloadFileAsync(new Uri(install), (path + @"\" + installer));
             }
             
         }
@@ -120,8 +142,7 @@ namespace Sharp_Updater
 
         private void button3_Click(object sender, EventArgs e)
         {
-            
-            Application.Exit();
+        	download.CancelAsync();
         }
 
     }
